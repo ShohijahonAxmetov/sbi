6c6714b218d61d61145c4a8806fc50dd2ae0eb47
Shohijahon Axmetov <shohijahonaxmetov@gmail.com>
2025-07-28 13:43:43 +0500

    first commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    private int $minPrice = 100000;
+    private int $maxPrice = 999999999;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'price' => fake()->randomFloat(2, $this->minPrice, $this->maxPrice),
+            'barcode' => fake()->regexify('^123456\d{8}$'),
+            'category_id' => Category::inRandomOrder()->first()->id,
+        ];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('categories', );
+Route::post('categories', );
+Route::get('categories/{id}', );
+Route::put('categories/{id}', );
+Route::delete('categories/{id}', );
+
+Route::get('products', );
+Route::post('products', );
+Route::get('products/{id}', );
+Route::put('products/{id}', );
+Route::delete('products/{id}', );
